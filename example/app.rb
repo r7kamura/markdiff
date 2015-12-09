9e18344929c7588064eb7b2b456d09b175b3ddf2
r7kamura <r7kamura@gmail.com>
2015-12-09 16:53:16 +0900

    Add demo sinatra app

---
+++ example/app.rb
@@ -0,0 +1,14 @@
+require "markdiff"
+require "redcarpet"
+require "sinatra"
+
+markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+
+get "/" do
+  if params[:before] && params[:after]
+    html_before = markdown.render(params[:before])
+    html_after = markdown.render(params[:after])
+    @diff = Markdiff::Differ.new.render(html_before, html_after)
+  end
+  erb :index
+end
