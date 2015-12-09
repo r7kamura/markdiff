ab161713586b332382f89ebf2a29912b140f3b08
r7kamura <r7kamura@gmail.com>
2015-12-09 16:15:19 +0900

    Fix bug on text diff operation

--- lib/markdiff/operations/text_diff_operation.rb
+++ lib/markdiff/operations/text_diff_operation.rb
@@ -20,7 +20,7 @@ module Markdiff
           if type == "-"
             before_elements[position] = "<del>#{element}</del>"
           else
-            before_elements[position] += "<ins>#{element}</ins>"
+            before_elements[position] = "#{before_elements[position]}<ins>#{element}</ins>"
           end
         end
         ::Nokogiri::HTML.fragment(before_elements.join(" "))
