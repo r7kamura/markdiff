6e94702eb12cc202c4c2875227fef3028b5887e6
Anders Mikkelsen <andersgm@outlook.dk>
2022-01-29 20:54:54 +0100

    Keep track of deletions

--- lib/markdiff/operations/text_diff_operation.rb
+++ lib/markdiff/operations/text_diff_operation.rb
@@ -15,14 +15,23 @@ module Markdiff
       def inserted_node
         before_elements = target_node.to_s.split(" ")
         after_elements = @after_node.to_s.split(" ")
+        deleted_positions = []
+
         ::Diff::LCS.diff(before_elements, after_elements).flatten(1).each do |operation|
           type, position, element = *operation
+
           if type == "-"
             before_elements[position] = %(<del class="del">#{element}</del>)
+            deleted_positions << position
           else
-            before_elements[position] = "<ins>#{element}</ins> #{before_elements[position]}"
+            if deleted_positions.include?(position)
+              before_elements[position] = "#{before_elements[position]}<ins>#{element}</ins>"
+            else
+              before_elements[position] = "<ins>#{element}</ins> #{before_elements[position]}"
+            end
           end
         end
+
         ::Nokogiri::HTML.fragment(before_elements.join(" "))
       end
 
